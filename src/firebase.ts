f0cf772037750172317ec1784773d7b092ca7015
Alexander Mayorga <alexandermayorga@users.noreply.github.com>
2024-01-03 05:58:56 -0600

    New Project
    Migrating to new project

--- src/firebase.ts
+++ src/firebase.ts
@@ -26,13 +26,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDL64q9KLVM0lkVsI47XCW5nPXXYYt6K5M",
-  authDomain: "evernote-clone-2023.firebaseapp.com",
-  projectId: "evernote-clone-2023",
-  storageBucket: "evernote-clone-2023.appspot.com",
-  messagingSenderId: "853893628954",
-  appId: "1:853893628954:web:bb688c33b64435579efda3",
-  measurementId: "G-8XVBQCQJTX",
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
 };
 
 // Initialize Firebase
